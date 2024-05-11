2d762e6b3601c39e1c8304e8e4a599246d4f5f71
Reazulrubon <rubonisdb1997@gmail,com>
2024-05-11 10:30:33 +0600

    add all file

---
+++ backend/about/addAbout.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type, X-Requested-With');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
+$con = new mysqli('localhost', 'root', '', 'techvilo');
+
+$title = $_POST['title'];
+$content = $_POST['content'];
+$btn = $_POST['btn'];
+
+
+$target_dir = "../image/";
+$target_file = $target_dir . basename($_FILES["photo"]["name"]);
+if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
+    $photo = $_FILES["photo"]["name"];
+} else {
+    $photo='';
+}
+
+$query = "INSERT INTO `about`(`photo`, `title`, `content`, `btn`) VALUES ('$photo','$title','$content','$btn')";
+if ($title != '') {
+    $con->query($query);
+    echo json_encode(['status'=>true]);
+}else{
+    echo json_encode(['status'=>false]);
+}
